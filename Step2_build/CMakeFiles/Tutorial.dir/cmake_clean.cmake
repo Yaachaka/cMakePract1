8b7e4bb7ea2e54d44ca068ddbc6845987153d9d6
Yaachaka <rosaarjuna@gmail.com>
2023-10-08 17:17:58 +0530

    Step2:Exercise1 completed.

---
+++ Step2_build/CMakeFiles/Tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorial.dir/tutorial.cxx.o"
+  "CMakeFiles/Tutorial.dir/tutorial.cxx.o.d"
+  "Tutorial.exe"
+  "Tutorial.pdb"
+  "libTutorial.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Step2_build/MathFunctions/CMakeFiles/MathFunctions.dir/MathFunctions.cxx.o.d
@@ -0,0 +1,4 @@
+MathFunctions/CMakeFiles/MathFunctions.dir/MathFunctions.cxx.o: \
+ /cygdrive/d/MG_Pract/cMakePracts/cMakePract1/Step2/MathFunctions/MathFunctions.cxx \
+ /cygdrive/d/MG_Pract/cMakePracts/cMakePract1/Step2/MathFunctions/MathFunctions.h \
+ /cygdrive/d/MG_Pract/cMakePracts/cMakePract1/Step2/MathFunctions/mysqrt.h
